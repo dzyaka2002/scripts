ed29d80a1f9d01c6bd00df9554a3ae3b4c6fe420
dzyaka2002 <108496922+dzyaka2002@users.noreply.github.com>
2024-10-20 01:41:35 +0300

    Update w10full

--- vms.sh
+++ vms.sh
@@ -67,7 +67,7 @@ echo 3. Ubuntu 22 Desktop, размер образа -  3.7 Гб.
 echo 4. Windows XP x86, размер образа -  1.1 Гб. 
 echo 5. Windows 7 x86, размер образа -  2.8 Гб.
 echo 6. Windows 7 x64, размер образа -  7.5 Гб.
-echo 7. Windows 10, размер образа -  7.5 Гб.
+echo 7. Windows 10, размер образа -  5.5 Гб.
 echo 8. Windows 2008 R2 Server, размер образа -  5.5 Гб.
 echo 9. Windows 2022 Server, размер образа -  4.4 Гб.
 echo 10. Windows 11, размер образа - 7 Гб.
@@ -81,7 +81,7 @@ read -p "Выберите необходимую ОС (введите нужну
 [ $var == 4 ] && l="https://www.googleapis.com/drive/v3/files/13jXUOQSpyEGXHzGecZY5Hg6khW0kR9xz/?key=AIzaSyBhmN7QmiDbqagUnq9gBGqA-yZYx5FmKMk&alt=media" && si=1172045824 && ovan=xp     
 [ $var == 5 ] && l="https://www.googleapis.com/drive/v3/files/15_T9Sl_yG4CWVbJMRpmEmLf4LrAubnW2/?key=AIzaSyBhmN7QmiDbqagUnq9gBGqA-yZYx5FmKMk&alt=media" && si=3067866112 && ovan=w7    
 [ $var == 6 ] && l="https://www.googleapis.com/drive/v3/files/1zstLks1X4-WX2Z47Q37rPjUtsI7VOgvj/?key=AIzaSyAt0u3A64Tl3_WCL4L_ihGY22p67RCI4wQ&alt=media" && si=7996131840 && ovan=w7f   
-[ $var == 7 ] && l="https://www.googleapis.com/drive/v3/files/1d56zmpWviYqTd5kkcq4PklsusD4lqx4a/?key=AIzaSyBhmN7QmiDbqagUnq9gBGqA-yZYx5FmKMk&alt=media" && si=8020727808 && ovan=w10   
+[ $var == 7 ] && l="https://www.googleapis.com/drive/v3/files/1CQM5tyz59cO6O1Nldui2KAhUD1JvOl-9/?key=AIzaSyBhmN7QmiDbqagUnq9gBGqA-yZYx5FmKMk&alt=media" && si=6003269632 && ovan=w10   
 [ $var == 8 ] && l="https://www.googleapis.com/drive/v3/files/1fTR6yQTjFLhfCb94hU7cUS1K3AAl4hxD/?key=AIzaSyCgtpLK0tle6L4ijI7PUtq35Xki1FGnaks&alt=media" && si=5930994688 && ovan=w2k8  
 [ $var == 9 ] && l="https://www.googleapis.com/drive/v3/files/1Q2R1S67p2sj_scScERfj4e5SHNWHpKdD/?key=AIzaSyCgtpLK0tle6L4ijI7PUtq35Xki1FGnaks&alt=media" && si=4712663040 && ovan=2k22 
 [ $var == 10 ] && l="https://www.googleapis.com/drive/v3/files/1ut6zencqNDPLJKm8HF82k93e37_Pgz1U/?key=AIzaSyAjS2du8B7ASFy_56T3T4pc5lRy-l7oaI8&alt=media" && si=7443925504 && ovan=w11  
